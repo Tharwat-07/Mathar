7676e58f24c82f59a2632dab0b661a207c392c3c
ahmed4end <35382418+ahmed4end@users.noreply.github.com>
2020-11-15 00:54:07 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,30 +2,18 @@ import eel
 from create import make
 import time
 import os 
-import subprocess
-
-
-#[init]
-fileName = ''
 
 eel.init('web')
 
 @eel.expose
-def getImages(*args):
-    global fileName
+def hi(*args):
+
     x,y,*_ = args
     data = list(zip(x, map(str,y.values())))
-    imgName = make(data)
-    fileName = imgName
-    imgsPaths = [ './output/'+imgName+'.png',  './output/'+ imgName+' answer.png']
-    return imgsPaths
 
-@eel.expose
-def openFileLoaction():
-        curPath= os.path.join(os.getcwd(), 'web', 'output', fileName+'.png')
-        subprocess.Popen(r'explorer /select,"'+curPath+'"')
-        return True
-
+    m = make(data)
+    print(m)
+    return m
 
 eel.start('index.html',port=86)
 
