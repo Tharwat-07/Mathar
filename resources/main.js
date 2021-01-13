c11b6f7a21f6cbd1e7d5a15696d21bfb9a2c59d7
Ahmed4end <as4end@gmail.com>
2021-01-14 01:55:18 +0200

    NEW tabs used - removed previous tabs for good.

--- resources/main.js
+++ resources/main.js
@@ -438,14 +438,6 @@ $(document).ready(function(){
 
     });
 
-
-    // transfer active class for tabs - taboo .
-    $(".nav-link").click(function() {
-
-        $('.nav-link').removeClass('active');
-        $(this).addClass('active');
-    });
-    
     
     //diabled old code for maintance.
     //save user inputs to html interface - temp .
@@ -526,9 +518,26 @@ $(document).ready(function(){
 
 // End docmennt ready
 
-// Trigger Tabs PLugin
-new Tabby('[data-tabs]');
+// tabs js engine code.
+const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
 
+for (let el of tabLinks) {
+  el.addEventListener("click", e => {
+    e.preventDefault();
+
+    document.querySelector(".tabs li.active").classList.remove("active");
+    document.querySelector(".tabs-panel.active").classList.remove("active");
+
+    const parentListItem = el.parentElement;
+    parentListItem.classList.add("active");
+    const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+    const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+    panel[0].classList.add("active");
+  });
+}
+//////////////////////
 
 
 //init funcs
